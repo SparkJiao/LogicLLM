6033acf34f3faae16f90d191c274d295ff5be9f7
sparkjiao <jiaofangkai@hotmail.com>
2022-04-05 10:30:15 +0800

    * Update new configs on A100.

--- scripts/roberta/reclor/run_pv9_v9.sh
+++ scripts/roberta/reclor/run_pv9_v9.sh
@@ -14,7 +14,24 @@
 
 
 #for seed in 42 43 44 45 4321; do
-for seed in 4321 46; do
+#for seed in 4321 46; do
+#  python -m torch.distributed.launch --nproc_per_node 2 --master_port 10000 trainer_base_fsdp_v1.py \
+#    seed=$seed -cp conf/roberta/reclor -cn merit_v1_pv9_v9_v3_0
+#done;
+
+#for seed in 42 43 44 45 4321; do
+#  python -m torch.distributed.launch --nproc_per_node 2 --master_port 10000 trainer_base_fsdp_v1.py \
+#    seed=$seed -cp conf/roberta/reclor -cn merit_v1_pv9_v9_v3_1
+#done;
+
+
+#for seed in 42 43 44 45 4321; do
+#  python -m torch.distributed.launch --nproc_per_node 2 --master_port 10000 trainer_base_fsdp_v1.py \
+#    seed=$seed -cp conf/roberta/reclor -cn merit_v1_pv9_v9_v4_0
+#done;
+
+
+for seed in 42 43 44 45 4321; do
   python -m torch.distributed.launch --nproc_per_node 2 --master_port 10000 trainer_base_fsdp_v1.py \
-    seed=$seed -cp conf/roberta/reclor -cn merit_v1_pv9_v9_v3_0
+    seed=$seed -cp conf/roberta/reclor -cn merit_v1_pv9_v9_v4_1
 done;
