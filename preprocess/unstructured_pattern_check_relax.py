8c4152983c36ed21ce49caeb2f4ac673adc63a85
sparkjiao <jiaofangkai@hotmail.com>
2022-10-31 15:42:35 +0800

    * Add path grouping based pre-processing.

--- preprocess/unstructured_pattern_check_relax.py
+++ preprocess/unstructured_pattern_check_relax.py
@@ -1,12 +1,17 @@
 import argparse
+import copy
 import json
 import pickle
 from collections import defaultdict
-from typing import Tuple, Dict, Union
+from multiprocessing import Pool
+from typing import Tuple, Dict, Union, List, Set
+from functools import partial
 
 from tqdm import tqdm
 
 RES: Union[Tuple[str, int], Tuple[str, str, int]]
+G: Dict[str, List[Tuple[str, str]]]
+EDGE2REL: Dict[str, str]
 
 
 def extract_path_and_parse(example):
@@ -68,12 +73,15 @@ def parse_args():
     parser.add_argument("--kg", type=str)
     parser.add_argument("--path_output_file", type=str, default=None)
     parser.add_argument("--rel_vocab", type=str, default=None)
+    parser.add_argument("--reid_examples", type=str, default=None)
+    parser.add_argument("--hop", type=int, default=2)
 
     args = parser.parse_args()
     return args
 
 
 def load_kg(kg_file):
+    g = defaultdict(list)
     edge2rel = {}
     rel2edge = defaultdict(list)
     rel_vocab = {}
@@ -86,12 +94,65 @@ def load_kg(kg_file):
             edge2rel[f"{s}\t{t}"] = rel
             rel2edge[rel].append(f"{s}\t{t}")
 
-            rel_vocab[rel] = len(rels)
-            rels.append(rel)
+            if rel not in rel_vocab:
+                rel_vocab[rel] = len(rels)
+                rels.append(rel)
+
+            g[s].append((rel, t))
 
             line = f.readline()
 
-    return edge2rel, rel2edge, rels, rel_vocab
+    return edge2rel, rel2edge, rels, rel_vocab, g
+
+
+def relax_w_hop(graph, edge2rel, hop: int = 2, num_workers: int = 16):
+    relaxed_edges = {}
+
+    def _init(_graph, _edge2rel):
+        global G
+        G = _graph
+        global EDGE2REL
+        EDGE2REL = _edge2rel
+
+    nodes = list(graph.keys())
+    with Pool(num_workers, initializer=_init, initargs=(graph, edge2rel)) as p:
+        _annotate = partial(dfs_proxy, hop=hop)
+        results = list(tqdm(
+            p.imap(_annotate, nodes, chunksize=32),
+            total=len(nodes),
+            desc="relaxing..."
+        ))
+
+    for res in tqdm(results, total=len(results), desc="updating edges"):
+        for _edge, _rel in res.items():
+            if _edge not in relaxed_edges and _edge not in edge2rel:
+                relaxed_edges[_edge] = _rel
+
+    return relaxed_edges
+
+
+def dfs_proxy(root, hop: int):
+    res = {}
+    node_vis = {root}
+    rel_path = []
+    dfs_find_path(root, root, hop, res, node_vis, rel_path)
+    return res
+
+
+def dfs_find_path(root, node, hop: int, res: Dict, node_vis: Set, rel_path: List[str]):
+    if len(rel_path) == hop:
+        edge = f"{root}\t{node}"
+        if edge not in EDGE2REL and edge not in res:
+            res[edge] = "+".join(rel_path)
+        return
+
+    for rel, nxt in G[node]:
+        if nxt in node_vis:
+            continue
+
+        node_vis_copy = copy.deepcopy(node_vis)
+        node_vis_copy.add(nxt)
+        dfs_find_path(root, nxt, hop, res, node_vis_copy, rel_path + [rel])
 
 
 def rearrange_example_id(examples):
@@ -114,7 +175,24 @@ def main():
     print(len(set([exp["id"] for exp in examples])))
     print(len(examples))
 
-    edge2rel, _, rels, rel_vocab = load_kg(args.kg)
+    edge2rel, _, rels, rel_vocab, g = load_kg(args.kg)
+
+    # for _ in range(args.hop - 1):
+    for _hop in range(2, args.hop + 1):
+        print(f"Before relaxation: {len(rels)}")
+
+        relaxed_edges = relax_w_hop(g, edge2rel, _hop, args.num_workers)
+        print(len(relaxed_edges))
+        edge2rel.update(relaxed_edges)
+        for _edge, _rel in tqdm(relaxed_edges.items(), total=len(relaxed_edges), desc="Updating rel vocab and graph G"):
+            if _rel not in rel_vocab:
+                rel_vocab[_rel] = len(rels)
+                rels.append(_rel)
+
+            # _s, _t = _edge.split("\t")
+            # g[_s].append((_rel, _t))
+
+        print(f"After relaxation: {len(rels)}")
 
     ent_path_set = defaultdict(list)
     rel_path_set = defaultdict(list)
@@ -149,10 +227,12 @@ def main():
             ent_path = ent_path.split("\t")
             direct_edge = f"{ent_path[0]}\t{ent_path[-1]}"
             rel_path_decode_id_b = rel_vocab[direct_edge] if direct_edge in rel_vocab else -1
+
             id2rel_path_decode_ids[example_id] = {
                 "input_a": rel_path_decode_id_a,
                 "input_b": rel_path_decode_id_b,
             }
+
             if rel_path_decode_id_b == -1:
                 no_direct_rel_cnt += 1
 
@@ -168,6 +248,9 @@ def main():
         pickle.dump(id2rel_path_decode_ids, open(args.path_output_file, "wb"))
     if args.rel_vocab is not None:
         pickle.dump(rels, open(args.rel_vocab, "wb"))  # id2rel_id
+    if args.reid_examples is not None:
+        data["examples"] = examples
+        pickle.dump(data, open(args.reid_examples, "wb"))
 
 
 if __name__ == '__main__':
