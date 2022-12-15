2af7b6f3d7d78028d38e2c911a83976d9dde9cd4
sparkjiao <jiaofangkai@hotmail.com>
2022-12-15 18:11:05 +0800

    * Add fine-tuning configs.

--- seed_multi_run.sh
+++ seed_multi_run.sh
@@ -2,15 +2,15 @@ conf=$1
 conf_name=$2
 num_rank=$3
 gpu=$4
-
+port=$5
 
 count=1
 for arg in "$@"; do
-  if [ "$count" -gt "4" ]; then
+  if [ "$count" -gt "5" ]; then
     if [ "$num_rank" -gt "1" ]; then
-      echo "CUDA_VISIBLE_DEVICES=$gpu python -m torch.distributed.run --nproc_per_node $num_rank --master_port 10082 trainer_base_fsdp_v3.py -cp $conf -cn $conf_name seed=${arg}"
+      echo "CUDA_VISIBLE_DEVICES=$gpu python -m torch.distributed.run --nproc_per_node $num_rank --master_port $port trainer_base_fsdp_v3.py -cp $conf -cn $conf_name seed=${arg}"
 
-      CUDA_VISIBLE_DEVICES=$gpu python -m torch.distributed.run --nproc_per_node $num_rank --master_port 10081 trainer_base_fsdp_v3.py -cp $conf -cn $conf_name seed=${arg}
+      CUDA_VISIBLE_DEVICES=$gpu python -m torch.distributed.run --nproc_per_node $num_rank --master_port $port trainer_base_fsdp_v3.py -cp $conf -cn $conf_name seed=${arg}
     else
       echo "CUDA_VISIBLE_DEVICES=$gpu python trainer_base_fsdp_v3.py -cp $conf -cn $conf_name seed=${arg}"
 
