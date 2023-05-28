92662219d7c57a2f05fba18a4537e4311af01a46
SparkJiao <jiaofangkai@hotmail.com>
2023-05-28 06:16:17 +0000

    sync from aws

--- trainer_base_ds_mul_aws.py
+++ trainer_base_ds_mul_aws.py
@@ -432,6 +432,7 @@ def main(cfg: DictConfig):
 
 if __name__ == "__main__":
     os.environ["HYDRA_FULL_ERROR"] = "1"
+    # os.system(f"df -h")
 
     hydra_formatted_args = []
     # convert the cli params added by torch.distributed.launch into Hydra format
