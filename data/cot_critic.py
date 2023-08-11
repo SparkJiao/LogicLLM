257172fd9616048af00d70fd84bfdbbcc7e2987b
sparkjiao <jiaofangkai@hotmail.com>
2023-08-11 10:12:20 +0800

    * Add actor model and llama2-70b qlora training config.

--- data/cot_critic.py
+++ data/cot_critic.py
@@ -101,17 +101,23 @@ class CoTPairData(Dataset):
 
 def vanilla_seq2seq_convertor(inputs, outputs, tokenizer: PreTrainedTokenizer, max_seq_length, padding="longest",
                               decoder_only: bool = False):
-    model_inputs = tokenizer(inputs, text_target=outputs, max_length=max_seq_length, padding=padding,
-                             truncation=True, return_tensors="pt")
     if decoder_only:
+        outputs = [inputs[b] + " " + o for b, o in enumerate(outputs)]
+
+        model_inputs = tokenizer(inputs, max_length=max_seq_length, padding=padding, truncation=True, return_tensors="pt")
         input_lens = model_inputs["input_ids"].ne(tokenizer.pad_token_id).sum(dim=1)
+
         model_inputs = tokenizer(outputs, max_length=max_seq_length, padding="max_length", truncation=True, return_tensors="pt")
         new_input_lens = model_inputs["input_ids"].ne(tokenizer.pad_token_id).sum(dim=1)
+
         input_lens = input_lens - input_lens.eq(new_input_lens).to(input_lens.dtype) * (input_lens // 2)
         input_lens = input_lens.to(torch.long)
         if tokenizer.padding_side == "left":
             input_lens = model_inputs["input_ids"].eq(tokenizer.pad_token_id).to(torch.long).sum(dim=1) + input_lens
         model_inputs["input_lens"] = input_lens
+    else:
+        model_inputs = tokenizer(inputs, text_target=outputs, max_length=max_seq_length, padding=padding,
+                                 truncation=True, return_tensors="pt")
 
     return model_inputs
 
@@ -277,17 +283,17 @@ class CoTActorRankingCollator:
 
 
 if __name__ == '__main__':
-
     from data.readers import LogiQAReaderV2
     from data.mp_inputs_process import LlamaDoubleHeadPpInputsProcess
 
     tokenizer = AutoTokenizer.from_pretrained("/export/home2/fangkai/pretrained-models/Llama-2-70b-hf")
 
-    dataset = CoTActorRankingDataset("experiments/llama2.7b.rw.lqv2cot.w4.A100.v1.0/lqv2cot_dev_zs_cot_2k_llama2_chat_70b_rewards.v1.0/test-checkpoint-1000/cot_w_feedback/cot_feedback.json",
-                                     tokenizer=tokenizer,
-                                     original_data="logiqa-v2/dev.txt",
-                                     read_func=LogiQAReaderV2(),
-                                     margin=8.0)
+    dataset = CoTActorRankingDataset(
+        "experiments/llama2.7b.rw.lqv2cot.w4.A100.v1.0/lqv2cot_dev_zs_cot_2k_llama2_chat_70b_rewards.v1.0/test-checkpoint-1000/cot_w_feedback/cot_feedback.json",
+        tokenizer=tokenizer,
+        original_data="logiqa-v2/dev.txt",
+        read_func=LogiQAReaderV2(),
+        margin=8.0)
 
     inputs_process = LlamaDoubleHeadPpInputsProcess()
 
